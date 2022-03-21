d6bc4535d39259c78c7d0a9d26af572b423ed5a7
Kirill Chernyshov <kchernys@cisco.com>
2022-03-21 17:53:30 +0100

    Introduce RiemannService

--- src/ctia/schemas/core.clj
+++ src/ctia/schemas/core.clj
@@ -5,8 +5,6 @@
    [ctia.schemas.services :as external-svc-fns]
    [ctia.schemas.utils :as csu]
    [ctia.store-service.schemas :refer [GetStoreFn]]
-   ;; [ctia.entity.event.schemas :as es]
-   ;; [ctia.lib.async :as la]
    [ctim.domain.id :as id]
    [ctim.schemas.bundle :as bundle]
    [ctim.schemas.common :as cos]
